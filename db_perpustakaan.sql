d878a9fb74419ec79d46a0506990d76b73e9c4ba
CahyaG <cahyagumilang0@gmail.com>
2022-05-06 20:15:14 +0700

    add db backup

--- db_perpustakaan.sql
+++ db_perpustakaan.sql
@@ -1,135 +1,100 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 27 Apr 2022 pada 09.06
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.3.9
+/*
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 10.4.24-MariaDB : Database - db_perpustakaan
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `author` */
 
---
--- Database: `db_perpustakaan`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `author`
---
+DROP TABLE IF EXISTS `author`;
 
 CREATE TABLE `author` (
-  `id_author` int(11) NOT NULL,
+  `id_author` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) NOT NULL,
-  `status` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_author`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `author`
---
+/*Data for the table `author` */
 
-INSERT INTO `author` (`id_author`, `nama`, `status`) VALUES
-(1, 'Richard Adkins', 'Senior'),
-(2, 'Pein Akatsuki', 'Senior'),
-(4, 'Ken Northwood', 'Pendatang Baru'),
-(5, 'ad', 'Pendatang Baru');
+insert  into `author`(`id_author`,`nama`,`status`) values 
+(1,'Richard Adkins','Senior'),
+(2,'Pein Akatsuki','Senior'),
+(4,'Ken Northwood','Senior'),
+(5,'ad','Pendatang Baru'),
+(6,'Cahya','Senior');
 
--- --------------------------------------------------------
+/*Table structure for table `buku` */
 
---
--- Struktur dari tabel `buku`
---
+DROP TABLE IF EXISTS `buku`;
 
 CREATE TABLE `buku` (
-  `id_buku` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL AUTO_INCREMENT,
   `judul_buku` varchar(100) NOT NULL,
   `penerbit` varchar(100) NOT NULL,
   `deskripsi` text NOT NULL,
   `status` varchar(50) NOT NULL,
-  `id_author` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_author` int(11) NOT NULL,
+  PRIMARY KEY (`id_buku`),
+  KEY `id_author` (`id_author`),
+  CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id_author`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `buku`
---
+/*Data for the table `buku` */
 
-INSERT INTO `buku` (`id_buku`, `judul_buku`, `penerbit`, `deskripsi`, `status`, `id_author`) VALUES
-(1, 'Alan Wade', 'Premium #', 'Cerita dibalik cerita', 'Best Seller', 2),
-(2, 'Awan', 'Akamedia', 'buku tentang cerita awan, hujan, langit', 'Best Seller', 1),
-(4, 'Puisi Patrick Star', 'Patrick', 'mawar itu biru, violet itu merah', '-', 2);
+insert  into `buku`(`id_buku`,`judul_buku`,`penerbit`,`deskripsi`,`status`,`id_author`) values 
+(1,'Alan Wade','Premium #','Cerita dibalik cerita','Best Seller',2),
+(2,'Awan','Akamedia','buku tentang cerita awan, hujan, langit','Best Seller',1),
+(4,'Puisi Patrick Star','Patrick','mawar itu biru, violet itu merah','Best Seller',2);
 
--- --------------------------------------------------------
+/*Table structure for table `member` */
 
---
--- Struktur dari tabel `member`
---
+DROP TABLE IF EXISTS `member`;
 
 CREATE TABLE `member` (
   `nim` varchar(50) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `jurusan` varchar(100) NOT NULL
+  `jurusan` varchar(100) NOT NULL,
+  PRIMARY KEY (`nim`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+/*Data for the table `member` */
 
---
--- Indeks untuk tabel `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id_author`);
+insert  into `member`(`nim`,`nama`,`jurusan`) values 
+('1234567','Cahgum','Ilmu Komputer'),
+('2003128','Cahya Gumilang','Ilmu Komputer');
 
---
--- Indeks untuk tabel `buku`
---
-ALTER TABLE `buku`
-  ADD PRIMARY KEY (`id_buku`),
-  ADD KEY `id_author` (`id_author`);
+/*Table structure for table `peminjaman` */
 
---
--- Indeks untuk tabel `member`
---
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`nim`);
+DROP TABLE IF EXISTS `peminjaman`;
 
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
+CREATE TABLE `peminjaman` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal_pinjam` datetime NOT NULL,
+  `tanggal_kembali` datetime DEFAULT NULL,
+  `id_buku` int(11) NOT NULL,
+  `nim` varchar(50) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nim` (`nim`),
+  KEY `id_buku` (`id_buku`),
+  CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `member` (`nim`) ON DELETE CASCADE,
+  CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
--- AUTO_INCREMENT untuk tabel `author`
---
-ALTER TABLE `author`
-  MODIFY `id_author` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*Data for the table `peminjaman` */
 
---
--- AUTO_INCREMENT untuk tabel `buku`
---
-ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+insert  into `peminjaman`(`id`,`tanggal_pinjam`,`tanggal_kembali`,`id_buku`,`nim`) values 
+(1,'2022-05-06 20:11:12','2022-05-06 20:11:15',2,'2003128');
 
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `buku`
---
-ALTER TABLE `buku`
-  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id_author`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
